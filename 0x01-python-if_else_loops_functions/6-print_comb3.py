11923e0d458a5209ae91715fdfc89de79abd04de
NomfundoNgwenya <ngwenyanomfundo09@gmail.com>
2023-12-12 11:33:51 -0800

    Updated 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for tens_digit in range(10):
-    for units_digit in range(tens_digit + 1, 10):
-        print("{:d}{:d}".format(tens_digit, units_digit), end=", " if (tens_digit, units_digit) != (8, 9) else "\n")
+for t in range(10):
+    for u in range(t + 1, 10):
+        print("{:d}{:d}".format(t, u), end=", " if (t, u) != (8, 9) else "\n")
